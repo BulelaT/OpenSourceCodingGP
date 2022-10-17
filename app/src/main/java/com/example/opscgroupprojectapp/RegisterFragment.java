088514509bd76d4f0cad2d607ec74def4f038b72
ST10119567 <st10119567@vcconnect.edu.za>
2022-10-17 12:39:24 +0200

    Authentication Done (Theorectically)

--- app/src/main/java/com/example/opscgroupprojectapp/RegisterFragment.java
+++ app/src/main/java/com/example/opscgroupprojectapp/RegisterFragment.java
@@ -2,6 +2,7 @@ package com.example.opscgroupprojectapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,6 +10,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.opscgroupprojectapp.Models.User_Model;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,18 +28,33 @@ public class RegisterFragment extends Fragment {
 
     EditText username, password, email, cPass;
     Button regBtn;
+    private FirebaseAuth mAuth;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
 
         View register = inflater.inflate(R.layout.fragment_register, container, false);
-
+        mAuth = FirebaseAuth.getInstance();
         username = register.findViewById(R.id.etUsername);
         password = register.findViewById(R.id.etPassword);
         email = register.findViewById(R.id.etGmail);
         cPass = register.findViewById(R.id.ConfirmPassword);
         regBtn= register.findViewById(R.id.regBtn);
+
+        
+
+        mAuth.createUserWithEmailAndPassword(email.toString(),password.toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        User_Model newUser = new User_Model(email.toString(),password.toString());
+
+                        FirebaseDatabase.getInstance().getReference("Users")
+                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                .setValue(newUser);
+                    }
+                });
 
         return register;
     }
